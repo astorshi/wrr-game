83d60238e8223af91a7d852002d39722c462bc6c
Astorshi <denpervyj@yandex.ru>
2021-08-12 08:31:14 +0300

    last release

--- server/app.js
+++ server/app.js
@@ -6,7 +6,6 @@ const cors = require("cors");
 //const { DB_HOST, DB_NAME, DB_PORT } = require("./src/db/config");
 //const { DB_HOST, DB_NAME, DB_PORT } = process.env;
 //const morgan = require('morgan');
-//const secretKey = "834579438534vlkdfvnfdlvn324n24543534" //require("crypto").randomBytes(64).toString("hex");
 const { connect } = require("./src/db/db");
 const unitRouter = require("./src/routes/unitRouter");
 
@@ -21,8 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use("/api/unit/", unitRouter);
-// app.use("/track", trackRouter);
-// res.sendStatus(200);
+
 
 
 app.listen(PORT, () => {
